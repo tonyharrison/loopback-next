acaded7f77b64448484f065a795ef896117bcdd3
Raymond Feng <enjoyjava@gmail.com>
2018-01-02 15:37:46 -0800

    feat(context): add @inject.tag to allow injection by a tag

--- packages/context/src/inject.ts
+++ packages/context/src/inject.ts
@@ -223,6 +223,22 @@ export namespace inject {
   ) {
     return inject(bindingKey || '', metadata, resolveAsOptions);
   };
+
+  /**
+   * Inject an array of values by a tag
+   * @param bindingTag Tag name or regex
+   * @example
+   * ```ts
+   * class AuthenticationManager {
+   *   constructor(
+   *     @inject.tag('authentication.strategy') public strategies: Strategy[],
+   *   ) { }
+   * }
+   * ```
+   */
+  export const tag = function injectTag(bindingTag: string | RegExp) {
+    return inject('', {tag: bindingTag}, resolveByTag);
+  };
 }
 
 function resolveAsGetter(ctx: Context, injection: Injection) {
@@ -283,6 +299,38 @@ export function describeInjectedArguments(
     method,
   );
   return meta || [];
+}
+
+function resolveByTag(
+  ctx: Context,
+  injection: Injection,
+  session?: ResolutionSession,
+) {
+  const tag: string | RegExp = injection.metadata!.tag;
+  const bindings = ctx.findByTag(tag);
+  const values: BoundValue[] = new Array(bindings.length);
+
+  // A closure to set a value by index
+  const valSetter = (i: number) => (val: BoundValue) =>
+    val instanceof RejectionError ? val : (values[i] = val);
+
+  let asyncResolvers: PromiseLike<BoundValue | RejectionError>[] = [];
+  // tslint:disable-next-line:prefer-for-of
+  for (let i = 0; i < bindings.length; i++) {
+    const val = bindings[i].getValue(ctx, session);
+    if (isPromise(val)) {
+      asyncResolvers.push(val.then(valSetter(i)));
+    } else {
+      values[i] = val;
+    }
+  }
+  if (asyncResolvers.length) {
+    return Promise.all(asyncResolvers).then(
+      vals => vals.find(v => v instanceof RejectionError) || values,
+    );
+  } else {
+    return values;
+  }
 }
 
 /**
